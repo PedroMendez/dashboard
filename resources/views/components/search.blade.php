da2a19e00672019b914b5a8c30b08021c868397b
Pedro <pedro@thefuzzyfish.com>
2019-12-01 14:57:36 -0300

    Changing search fade-in to bounce-in

--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -1,9 +1,9 @@
-<div class="modal modal-search fade-in" id="searchModal" tabindex="-1" role="dialog" aria-labelledby="searchModal" aria-hidden="true">
+<div class="modal modal-search bounce-in-from-left" id="searchModal" tabindex="-1" role="dialog" aria-labelledby="searchModal" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="SEARCH">
-                <button onclick="document.getElementById('searchModal').classList.add('fade-out'); let timeout = window.setTimeout(cleaning, 700); function cleaning() { let search = document.getElementById('searchModal'); search.classList.toggle('show'); search.classList.toggle('fade-out'); clearTimeout(timeout); };" type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <button onclick="document.getElementById('searchModal').classList.add('out-to-left'); let timeout = window.setTimeout(cleaning, 700); function cleaning() { let search = document.getElementById('searchModal'); search.classList.toggle('show'); search.classList.toggle('out-to-left'); clearTimeout(timeout); };" type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <i class="tim-icons icon-simple-remove"></i>
                 </button>
             </div>
